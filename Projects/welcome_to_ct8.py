220f4161f2c5657857bc72467eab7e7dbab43d2d
Ellepeterson17 <ellepeterson@seattleacademy.org>
2025-12-05 16:38:20 +0000

    Test

--- Projects/welcome_to_ct8.py
+++ Projects/welcome_to_ct8.py
@@ -27,4 +27,6 @@ print("\n\n")
                                                                                           
                                   
 print("Now it's your turn:")
-print("Find this line (line 30) in the welcome_to_ct8.py file, then change the message to say a fact about you.")                                  
+print("I have 2 dogs.")          
+print("I like to bake.")   
+print("I play tennis.")                     
---
+++ jokes.py
@@ -0,0 +1,5 @@
+print("What do you call a bear with no teeth?")
+input()
+print("A gummy bear")
+input()
+print("")
